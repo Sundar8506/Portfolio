79d40201832d45337471ff36c52688aefbbd668e
Sundhareshan <sundhareshan8506@mountzion.ac.in>
2025-04-10 07:55:23 +0530

    final process
    final

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default function About(){
       }
     return <section className="flex flex-col md:flex-row bg-secondary px-5" id="about">
         <div className="py-5 md:w-1/2">
-            <img src={AboutImg}/>
+            <img src={AboutImg} alt="sunder"/>
         </div>
         <div className="md:w-1/2 flex justify-center ">
            <div className="flex flex-col justify-center ">
