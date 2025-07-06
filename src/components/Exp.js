3433408e47b7a80d40a0fa03cefb28b1ec1b03fe
Sundhareshan <sundhareshan8506@mountzion.ac.in>
2025-07-06 22:26:23 +0530

    6-7

--- src/components/Exp.js
+++ src/components/Exp.js
@@ -50,7 +50,7 @@ const Exp = () => {
   ];
 
   return (
-    <section id="experience" className="py-16 px-6 md:px-16 lg:px-32 bg-white dark:bg-[#00052a] transition-colors duration-500">
+    <section id="experience" className="py-16 px-6 md:px-16 lg:px-32 bg-black dark:bg-[#00052a] transition-colors duration-500">
       <div className="max-w-7xl mx-auto text-center">
         <h2 className="text-4xl font-bold mb-12 text-gray-900 dark:text-white hover:text-[#EFB036] transition">Experience</h2>
 
@@ -83,7 +83,7 @@ const Exp = () => {
                   <p className="text-sm text-gray-500 dark:text-gray-300 mb-2">{item.para}</p>
                   <p className="text-sm leading-relaxed text-gray-700 dark:text-gray-200">{item.desc}</p>
 
-                  <div className="absolute top-4 right-4 text-xs font-medium px-3 py-1 bg-[#EFB036] text-white rounded-full shadow-md">
+                  <div className="absolute top-4 right-4 text-xs font-medium px-3 py-1 bg-[#EFB036] rounded-full shadow-md">
                     {item.skills}
                   </div>
                 </div>
